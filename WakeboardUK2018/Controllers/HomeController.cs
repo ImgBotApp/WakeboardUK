d3325b4d3824fd7e6d3eaf1eca9892a0e0429c04
Jeremy Nevill <jeremy@nevill.net>
2017-10-28 12:45:09 +0000

    Much mods in the tidying department.

--- WakeboardUK2018/Controllers/HomeController.cs
+++ WakeboardUK2018/Controllers/HomeController.cs
@@ -17,15 +17,16 @@ namespace WakeboardUK2018.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
 
+        public IActionResult Membership()
+        {
             return View();
         }
 
