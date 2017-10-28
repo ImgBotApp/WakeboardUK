b2ebecfd7f3cade8b11be92c2bf2e93056c1f502
Jeremy Nevill <jeremy@nevill.net>
2017-10-28 09:23:06 +0000

    Models added.

---
+++ WakeboardUK2018/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WakeboardUK2018.Models
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
