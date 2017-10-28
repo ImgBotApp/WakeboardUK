33beae2a5f575de28ec9c1c9d3d3a1c328920b87
Jeremy Nevill <jeremy@nevill.net>
2017-10-28 09:18:21 +0000

    .net core 2.0 second attempt.

--- WakeboardUK2018/Models/ApplicationUser.cs
+++ WakeboardUK2018/Models/ApplicationUser.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-namespace WakeboardUK.Web.Models
+namespace WakeboardUK2018.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
--- WakeboardUK2018/Startup.cs
+++ WakeboardUK2018/Startup.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WakeboardUK2018.Data;
+using WakeboardUK2018.Models;
+using WakeboardUK2018.Services;
 
 namespace WakeboardUK2018
 {
@@ -21,6 +26,16 @@ namespace WakeboardUK2018
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
             services.AddMvc();
         }
 
@@ -30,6 +45,8 @@ namespace WakeboardUK2018
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -37,6 +54,8 @@ namespace WakeboardUK2018
             }
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
